c6828526f74844697e366c078e7e444ada419191
Sreekanth <sreekanthjobs9010@gmail.com>
2024-06-09 19:09:10 +0530

    add function defination example

---
+++ functionDefination.js
@@ -0,0 +1,9 @@
+//here caluculateSquare is the name of the function
+function caluculateSquare(number){
+    //number is input that the function expecting . also known as parameters
+    return number**2;
+    // return the output 
+    //Here parameters and output are non mandatory for a function . If no return statement is mentioned then default return is "undefined"
+}
+
+console.log(caluculateSquare(3))
